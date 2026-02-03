7061446c59b1751e4e767b6c2f71195d8d17def2
Arman Tiwari <tiwariar279@gmail.com>
2026-02-03 10:19:55 +0530

    Add TaskSchedulingEnv class for task scheduling

--- Streamlit File/app.py
+++ Streamlit File/app.py
@@ -1,1 +1,14 @@
+import pandas as pd
+import numpy as np
+import os 
 
+class TaskSchedulingEnv(gym.Env):
+    def __init__(self, tasks, team, skill_encoder):
+        super(TaskSchedulingEnv, self).__init__()
+        self.tasks = tasks
+        self.team = team
+        self.skill_encoder = skill_encoder
+        self.action_space = gym.spaces.Discrete(len(team))
+        self.observation_space = gym.spaces.Box(low=-np.inf, high=np.inf, shape=(4 + len(team),), dtype=np.float32)
+        self.current_task_idx = 0
+        self.state = self._get_state()
